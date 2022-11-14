b94b19b3bedd118fe8c976db2348e26db112a015
JuanQ1226 <juan.quintana5@upr.edu>
2022-11-14 16:23:23 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <a className="Resume-Link"
             href="https://docs.google.com/document/d/1gOQxj1Ook9QBmz1vjTjJW-tSYr34PhcR/edit?usp=share_link&ouid=107943270788566552480&rtpof=true&sd=true"
             target="_blank"
+            rel='noopener noreferrer'
         >
             My Resume
         </a>
